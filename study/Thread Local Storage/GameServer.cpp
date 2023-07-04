517d31a2f9c585b13d358bc31820824138ed84fd
강병우 <peace0096@naver.com>
2023-07-04 14:49:01 +0900

    Create GameServer.cpp

---
+++ study/Thread Local Storage/GameServer.cpp
@@ -0,0 +1,36 @@
+#include "pch.h"
+#include <iostream>
+#include "CorePch.h"
+#include <thread>
+#include <atomic>
+#include <mutex>
+#include <windows.h>
+#include <future>
+
+// _declspec(thread) int32 value
+thread_local int32 LThreadId = 0;
+
+void ThreadMain(int32 threadId)
+{
+	LThreadId = threadId;
+
+	while (true)
+	{
+		cout << "Hi I am Thread" << LThreadId << endl;
+		this_thread::sleep_for(1s);
+	}
+}
+
+int main()
+{
+	vector<thread> threads;
+
+	for (int32 i = 0; i < 10; i++)
+	{
+		int32 threadId = i + 1;
+		threads.push_back(thread(ThreadMain, threadId));
+	}
+
+	for (thread& t : threads)
+		t.join();
+}
