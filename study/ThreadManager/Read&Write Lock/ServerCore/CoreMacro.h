c1977b8b5efe80f27deb888d79ef9ef59b24852f
강병우 <peace0096@naver.com>
2023-07-07 17:11:23 +0900

    Add files via upload

---
+++ study/ThreadManager/Read&Write Lock/ServerCore/CoreMacro.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#define OUT
+
+/*----------------------
+	Lock
+----------------------*/
+
+#define USE_MANY_LOCKS(count)	Lock _locks[count];
+#define USE_LOCK				USE_MANY_LOCKS(1);
+#define READ_LOCK_IDX(idx)		ReadLockGuard readLockGuard_##idx(_locks[idx]);
+#define READ_LOCK				READ_LOCK_IDX(0);
+#define WRITE_LOCK_IDX(idx)		WriteLockGuard writeLockGuard_##idx(_locks[idx]);
+#define WRITE_LOCK				WRITE_LOCK_IDX(0);
+/*----------------------
+	Crash
+----------------------*/
+#define CRASH(cause)						\
+{											\
+	uint32* crash = nullptr;				\
+	_Analysis_assume_(crash != nullptr);	\
+	*crash = 0XDEADBEEF;					\
+}
+
+#define ASSERT_CRASH(expr)					\
+{											\
+	if (!(expr))							\
+	{										\
+		CRASH("ASSERT_CRASH");				\
+		_Analysis_assume_(expr);			\
+	}										\
+}
---
+++ study/ThreadManager/Read&Write Lock/ServerCore/Lock.cpp
@@ -0,0 +1,86 @@
+#include "pch.h"
+#include "Lock.h"
+
+void Lock::WriteLock()
+{
+	// 동일한 쓰레드가 소유하고 있다면 무조건 성공
+	const uint32 lockThreadId = (_lockFlag.load() & WRITE_THREAD_MASK) >> 16;
+	if (LThreadId == lockThreadId)
+	{
+		_writeCount++;
+		return;
+	}
+
+	// 아무도 소유 및 공유하고 있지 않을 때, 경합해서 소유권을 얻는다.
+	// 32비트 Flag 모두 비어 있을 때!
+	// 16비트 이동 후, WriteFlag 작성
+	const int64 beginTick = ::GetTickCount64();
+	const uint32 desired = ((LThreadId << 16) & WRITE_THREAD_MASK);
+	
+	while (true)
+	{
+		for (uint32 spinCount = 0; spinCount < MAX_SPIN_COUNT; spinCount++)
+		{
+			uint32 expected = EMPTY_FLAG;	// Lock이 풀려있을 때의 값(비어 있음)
+			if (_lockFlag.compare_exchange_strong(OUT expected, desired))
+			{
+				// Write 점유 !
+				_writeCount++;
+				return;
+			}
+		}
+
+		// 시간 초과
+		if (::GetTickCount64() - beginTick >= ACQUIRE_TIMEOUT_TICK)
+			CRASH("LOCK_TIMEOUT");
+
+
+		// 잠깐 쉬기
+		this_thread::yield();
+	}
+}
+
+void Lock::WriteUnlock()
+{
+	// ReadLock을 다 풀기 전에는 WriteUnLock 불가능
+	if ((_lockFlag.load() & READ_COUNT_MASK) != 0)
+		CRASH("INVALID_UNLOCK_ORDER");
+
+	const int32 lockCount = --_writeCount;
+	if (lockCount == 0)
+		_lockFlag.store(EMPTY_FLAG);
+}
+
+void Lock::ReadLock()
+{
+	// 동일한 쓰레드가 소유하고 있다면 무조건 성공
+	const uint32 lockThreadId = (_lockFlag.load() & WRITE_THREAD_MASK) >> 16;
+	if (LThreadId == lockThreadId)
+	{
+		_lockFlag.fetch_add(1);
+		return;
+	}
+
+	// 아무도 소유하고 있지 않을 때 경합해서 공유 카운트를 올린다.
+	const int64 beginTick = ::GetTickCount64();
+	while (true)
+	{
+		for (uint32 spinCount = 0; spinCount < MAX_SPIN_COUNT; spinCount++)
+		{
+			uint32 expected = (_lockFlag.load() & READ_COUNT_MASK);
+			if (_lockFlag.compare_exchange_strong(OUT expected, expected + 1))
+				return;
+		}
+
+		if (::GetTickCount64() - beginTick >= ACQUIRE_TIMEOUT_TICK)
+			CRASH("LOCK_TIMEOUT");
+
+		this_thread::yield();
+	}
+}
+
+void Lock::ReadUnlock()
+{
+	if ((_lockFlag.fetch_sub(1) & READ_COUNT_MASK) == 0)
+		CRASH("MULTIPLE_UNLOCK");
+}
