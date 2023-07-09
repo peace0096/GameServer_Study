d0736e40e2220d41ac1cf7beea1af2a854631ad3
강병우 <peace0096@naver.com>
2023-07-09 16:47:17 +0900

    RefCount upload
    스마트포인터를 활용하여 객체 멤버가 다른 메모리를 가리키고 있을 때, RefCount로 관리하는 소스코드입니다.

---
+++ study/ReferenceCounting/GameServer/GameServer.cpp
@@ -0,0 +1,91 @@
+﻿#include "pch.h"
+#include <iostream>
+#include "CorePch.h"
+#include <thread>
+#include <atomic>
+#include <mutex>
+#include <windows.h>
+#include <future>
+#include "ThreadManager.h"
+
+#include "RefCountable.h"
+
+class Wraight : public RefCountable
+{
+public:
+	int _hp = 150;
+	int _poxX = 0;
+	int _poxY = 0;
+};
+
+using WraightRef = TSharedPtr<Wraight>;
+
+
+class Missile : public RefCountable
+{
+public:
+	void SetTarget(WraightRef target)
+	{
+		_target = target;
+		// target->AddRef();
+	}
+
+	bool Update()
+	{
+		if (_target == nullptr)
+			return true;
+		int posX = _target->_poxX;
+		int posY = _target->_poxY;
+
+		// TODO : 쫓아간다.
+
+		if (_target->_hp == 0)
+		{
+			_target->ReleaseRef();
+			_target = nullptr;
+			return true;
+		}
+			
+		return false;
+	}
+
+	WraightRef _target = nullptr;
+
+};
+
+
+using MissileRef = TSharedPtr<Missile>;
+
+int main()
+{
+	WraightRef wraight(new Wraight());
+	wraight->ReleaseRef();
+	MissileRef missile(new Missile());
+	missile->ReleaseRef();
+
+	missile->SetTarget(wraight);
+
+	// 레이스가 피격당함
+	wraight->_hp = 0;
+	// wraight->ReleaseRef();
+	wraight = WraightRef(nullptr);	// TSharedPtr& operator= 에서 Release()를 통해 알아서 메모리 비워줌
+	// delete wraight;	// 어 이러면 미사일이 가리키는 포인터도 삭제되지 않나..
+
+	while (true)
+	{
+		if (missile)
+		{
+			if (missile->Update())
+			{
+				missile->ReleaseRef();
+				missile = nullptr;
+
+			}
+		}
+	}
+
+	missile->ReleaseRef();
+	missile = nullptr;
+	//delete missile;
+}
+
