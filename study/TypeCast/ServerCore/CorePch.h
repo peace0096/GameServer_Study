1a48b6ddc933dbaa025e61188369e5c9e9b08994
강병우 <peace0096@naver.com>
2023-07-16 13:55:14 +0900

    헤더 파일 업로드

---
+++ study/TypeCast/ServerCore/CorePch.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "Types.h"
+#include "CoreGlobal.h"
+#include "CoreTLS.h"
+#include "CoreMacro.h"
+#include "Container.h"
+
+#include <Windows.h>
+#include <iostream>
+using namespace std;
+
+#include "Lock.h"
+
+#include "ObjectPool.h"
+#include "TypeCast.h"
